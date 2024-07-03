5284f9bf4b8d9e29e09ede366e0a68dc5b1a882a
hnonoyy <hnono0210@gmail.com>
2024-07-03 14:22:58 +0900

    Hello_실습

---
+++ Hello_001/src/com/callor/hello/HelloA.java
@@ -0,0 +1,17 @@
+package com.callor.hello;
+
+public class HelloA {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Korea");
+		System.out.println(30 + 40);
+		System.out.println(30 + 40);
+		System.out.println(30 + 40);
+		System.out.println(30 + 40);
+		System.out.println(30 + 40);
+		
+		int n1 = 30;
+		int n2 = 40;
+	}
+
+}
